spsforks/android-bionic-libc

Fix GNU/POSIX basename headers.
Including glibc's <libgen.h> will result in the user getting the POSIX
version of basename always, regardless of when it is included relative
to <string.h>. Prior to this patch, our implementation would result in
the one that's included first winning.

Bug: http://b/25459151
Change-Id: Id4aaf1670dad317d6bbc05763a84ee87596e8e59

tests/Android.mk
@@ -64,6 +64,7 @@ libBionicStandardTests_src_files := \
     inttypes_test.cpp \
     libc_logging_test.cpp \
     libgen_test.cpp \
+    libgen_gnu_basename_test.cpp \
     locale_test.cpp \
     malloc_test.cpp \
     math_test.cpp \

VRToxin-AOSP/android_bionic

tests/Android.mk
@@ -63,6 +63,7 @@ libBionicStandardTests_src_files := \
     getcwd_test.cpp \
     inttypes_test.cpp \
     libc_logging_test.cpp \
+    libgen_basename_test.cpp \
     libgen_test.cpp \
     locale_test.cpp \
     malloc_test.cpp \